eb692ddd480a315cf3f31f512a0be05269c0a05f
Haidar Khan <haidark@sdaia.gov.sa>
2024-07-09 15:03:05 -0400

    initialize mathquiz

---
+++ zero_sum_eval/games/mathquiz/mathquiz_game.py
@@ -0,0 +1,125 @@
+from zero_sum_eval.game_state import GameState
+
+class MathQuizGame(GameState):
+    def __init__(self, roles=None, environment=None, context=None):
+        super().__init__()
+        self.environment = environment
+        self.roles = self.get_next_roles(self.environment) if roles is None else roles
+        self.context = context if context is not None else {"history": [], "message": None}
+
+    def initialize(self, environment, context=None ,roles=None):
+        return MathQuizGame(
+            roles=roles,
+            environment=environment,
+            context=context
+        )
+
+    def update_game(self, move):
+        new_context = self.context.copy()
+
+        try:
+            chess_move = self.board.parse_san(move)
+            if self.board.is_legal(chess_move):
+                self.board.push(chess_move)
+                new_context['history'].append(f"{move}")
+                new_context['message'] = None 
+                #maybe fix message here
+            else:
+                new_context['message'] = f"Your move {move} is an illegal move"
+        except ValueError as e:
+            new_context['message'] = f"Your move {move} caused an error: {str(e)} "
+
+        new_environment = self.board.fen()
+        return self.initialize(
+            environment=new_environment,
+            context=new_context
+        )
+
+    def query_game(self):  
+        new_context = self.context.copy()
+        new_roles = [self.roles[0]]
+        msg = self.validate_game() 
+        new_context['message'] = msg if msg is not None else f"You will move as {self.get_next_roles(self.environment)[0]}" 
+
+        return self.initialize(
+            environment=self.environment,
+            context=new_context,
+            roles=new_roles
+        )
+
+    def validate_game(self):
+        if self.board.is_checkmate():
+            return "Checkmate"
+        elif self.board.is_stalemate():
+            return "Stalemate"
+        elif self.board.is_insufficient_material():
+            return "Insufficient material"
+        elif self.board.is_seventyfive_moves():
+            return "75-move rule"
+        elif self.board.is_fivefold_repetition():
+            return "Fivefold repetition"
+        elif not self.board.is_valid():
+            return "Invalid"
+        else:
+            return self.context['message']
+
+    def get_next_roles(self, fen):
+        turn = fen.split()[1]  # 'w' or 'b'
+        return ['White', 'Black'] if turn == 'w' else ['Black', 'White']
+
+    def formatted_move_history(self):
+        history = self.context['history']
+        formatted_history = ""
+        moves = len(history)//2+1
+        for i in range(1, moves+1):
+            j = (i-1)*2
+            formatted_history+=f"{i}."
+            if j < len(history):
+                formatted_history+=f"{history[j]} "
+            if j+1 < len(history):
+                formatted_history+=f"{history[j+1]} "
+        return formatted_history.strip()
+
+    def export(self):
+        return {
+            'roles': self.roles,
+            'environment': self.environment,
+            'context': self.context
+        }
+    
+    def display(self):
+        display_str = f"Role to Act: {self.roles[0]}\nMessage: {self.context['message']}\n"
+        display_str += f"{self.formatted_move_history()}\n"
+        display_str += f"{self.board}\n"
+        return display_str
+
+
+if __name__ == "__main__":
+    chess_game = ChessGame().initialize(chess.Board().fen())
+    print(chess_game.export())
+
+    # 1. e4 e5
+    chess_game = chess_game.update_game("e4")
+    print(chess_game.export())
+
+    print(chess_game.query_game().export())
+    chess_game = chess_game.update_game("e5")
+    print(chess_game.export())
+
+    # 2. Nf3 Nc6
+    chess_game = chess_game.update_game("Nf3")
+    print(chess_game.query_game().export())
+    chess_game = chess_game.update_game("Nc6")
+    print(chess_game.export())
+
+    # 3. Nxe5
+    chess_game = chess_game.update_game("Nxe5")
+    print(chess_game.export())
+
+    validation_result = chess_game.validate_game()
+    if validation_result:
+        print(f"Game validation result: {validation_result}")
+    else:
+        print("Game is valid.")
+
+
---
+++ zero_sum_eval/games/mathquiz/mathquiz_player.py
@@ -0,0 +1,140 @@
+# I took inspiration from https://github.com/carlini/chess-llm and https://github.com/mlabonne/chessllm
+# Shout out to the maintainers and authors of these repositories!
+
+from zero_sum_eval.player import Player
+import dspy
+from dspy.primitives.assertions import assert_transform_module, backtrack_handler
+from dspy.teleprompt import LabeledFewShot, BootstrapFewShot, MIPROv2, BootstrapFewShotWithRandomSearch
+import copy
+import chess
+from chess import IllegalMoveError, InvalidMoveError, AmbiguousMoveError
+import functools, json
+from random import shuffle
+
+# TODO: add support for resigning
+
+def validate_move(example, prediction, trace=None):
+    pred_move = prediction.move
+    true_move = example.move
+    board_state = example.board_state
+    board = chess.Board(board_state)
+    if true_move is not None and pred_move == true_move:
+        return 1
+    elif board.is_legal(board.parse_san(pred_move)):
+        return 0
+    else:
+        return -1
+
+class NextMove(dspy.Signature):
+    """Given a board state, role, and move history, produce the next best valid move"""
+    
+    board_state = dspy.InputField(desc="FEN formatted current board state")
+    role = dspy.InputField(desc="role of the player making the next move")
+    history = dspy.InputField(desc="move history")
+    move = dspy.OutputField(desc="a valid SAN formatted move without move number or elipses")
+
+
+class ChessCoT(dspy.Module):
+    def __init__(self):
+        super().__init__()
+        self.cot_move = dspy.ChainOfThought(NextMove)
+
+
+    def forward(self, board_state, role, history):
+        cot_out = self.cot_move(board_state=board_state,
+                                role=role,
+                                history=history)
+        cot_out.move = cot_out.move.replace(".", "")
+        try:
+            board = chess.Board(board_state)
+            move = board.parse_san(cot_out.move)
+        except IllegalMoveError:
+            dspy.Suggest(
+                False,
+                f"{cot_out.move} is an illegal move, choose a different move.",
+            )
+        except InvalidMoveError:
+            dspy.Suggest(
+                False,
+                f"{cot_out.move} is an invalid move, choose a different move."
+            )
+        except AmbiguousMoveError:
+            dspy.Suggest(
+                False,
+                f"{cot_out.move} is an ambiguous move, choose a different move."
+            )
+        return cot_out
+
+class ChessPlayer(Player):
+    def __init__(self, llm_model, max_tries=4, **kwargs):
+        super().__init__(**kwargs)
+        self.llm_model = llm_model
+        self.max_tries = max_tries
+        self.module = assert_transform_module(ChessCoT(), functools.partial(backtrack_handler, max_backtracks=max_tries))
+        self.optimized_module = self.optimize_prompts() if self.optimize else None
+        dspy.configure(trace=[])
+
+    def make_move(self, game_state):
+        """
+        Abstract method for making a move based on the current game state.
+        
+        Parameters:
+        game_state (GameState): The current state of the game
+        
+        Returns:
+        str: The move made by the player
+        """
+        export = game_state.export()
+        with dspy.context(lm=self.llm_model):
+            if self.optimize:
+                trace = self.optimized_module(board_state=export['environment'],
+                                              role=export['roles'][0], 
+                                              history=game_state.formatted_move_history())
+            else:
+                trace = self.module(board_state=export['environment'],
+                                    role=export['roles'][0], 
+                                    history=game_state.formatted_move_history())
+        return trace.move
+    
+    def optimize_prompts(self):
+        filename = 'data/chess/stockfish_examples.jsonl'
+        dataset = self.create_dataset(filename)
+        shuffle(dataset)
+        # config = dict(max_bootstrapped_demos=4, max_labeled_demos=16)
+        # teleprompter = BootstrapFewShot(metric=validate_move, **config)
+        with dspy.context(lm=self.llm_model):
+            
+            teleprompter = MIPROv2(metric=validate_move, prompt_model=self.llm_model, task_model=self.llm_model,
+                                   num_candidates=5, minibatch_size=20, minibatch_full_eval_steps=10, 
+                                   verbose=True)
+            return teleprompter.compile(self.module, max_bootstrapped_demos=1, max_labeled_demos=1, 
+                                        trainset=dataset, eval_kwargs={})
+
+    def load_examples(self, filename):
+        examples = []
+        with open(filename, 'r') as f:
+            for line in f:
+                examples.append(json.loads(line))
+        return examples
+
+    def create_dataset(self, filename):
+        examples = self.load_examples(filename)
+        dataset = []
+        for example in [examples[i] for i in range(0, len(examples), len(examples)//10)]:
+            if self.role =="White": # white to move
+                if not example['turn']:
+                    continue
+            else:                   # black to move
+                if example['turn']:
+                    continue
+            example = dspy.Example(board_state=example['board_state'],
+                                   role=f"{self.role}",
+                                   history=example['history'],
+                                   move=example['move']
+                                   ).with_inputs("board_state", "role", "history")
+            dataset.append(example)
+        return dataset
+    
+
+        
+    
