055dd68d1f8205d897cea5594cdae38c7c6ff5ef
Hisham Alyahya <Hishamaalyahya@gmail.com>
2024-09-28 19:16:05 +0000

    Add multithreading in GamePoolManager

--- zero_sum_eval/managers/game_pool_manager.py
+++ zero_sum_eval/managers/game_pool_manager.py
@@ -1,5 +1,6 @@
 
 from collections import defaultdict
+from concurrent.futures import ThreadPoolExecutor, as_completed
 from copy import copy
 from glob import glob
 import json
@@ -62,72 +63,82 @@ class GamePoolManager:
         print(self.match_freq)
         return min(self.match_freq.keys(), key=lambda k: self.match_freq[k])
 
+    def run_match(self, lms):
+        turn_dir = os.path.join(self.output_dir, f"matches/{lms[0]}_vs_{lms[1]}_{int(time.time())}")
+        os.makedirs(turn_dir, exist_ok=True)
+
+        game_manager = self._build_game_manager(lms=lms, turn_dir=turn_dir)
+
+        assert len(lms) == len(game_manager.games[-1].roles), "The number of matched LMs must be the same as the number of players required in the game."
+        
+        self.logger.info(" VS ".join(lms))
+        
+        final_game_state = game_manager.start()
+        
+        # Get which LM's turn the game stopped at
+        cur_lm_turn = self.roles[final_game_state.roles[0]]
+        gm_message = final_game_state.validate_game()
+        if gm_message in game_manager.win_conditions:
+            result_a = 1 if cur_lm_turn == lms[0] else 0
+            self.logger.info(f"Match {cur_lm_turn} won! GM message: {gm_message}")
+        elif gm_message in game_manager.draw_conditions:
+            result_a = 0.5
+            self.logger.info(f"Match ended in a draw! GM message: {gm_message}")
+        else:
+            # Loss to current LM because they made the state invalid (exceeded max_tries)
+            result_a = 0 if cur_lm_turn == lms[0] else 1
+            self.logger.info(f"Match {cur_lm_turn} lost! GM message: {gm_message}")
+        
+        elos_delta = {}
+        if result_a == 1:
+            elos_delta[lms[0]] = [0, 1]
+            elos_delta[lms[1]] = [0, -1]
+        elif result_a == 0:
+            elos_delta[lms[0]] = [0, -1]
+            elos_delta[lms[1]] = [0, 1]
+        else:
+            elos_delta[lms[0]] = [0, 0]
+            elos_delta[lms[1]] = [0, 0]
+
+        with open(os.path.join(turn_dir, "results.json"), mode='w', newline='') as f:
+            obj = {
+                lms[0]: {
+                    "elos_delta": elos_delta[lms[0]],
+                    "result": result_a
+                },
+                lms[1]: {
+                    "elos_delta": elos_delta[lms[1]],
+                    "result": 1 - result_a
+                }
+            }
+            json.dump(obj, f)
+        
+        return lms, result_a
+
     def start(self):
         self.logger.info("Let the games begin!")
 
         os.makedirs(os.path.join(self.output_dir, "leaderboard_history"), exist_ok=True)
-        for _ in range(self.max_matches):
-            # Get next matchup
-            lms = self.get_next_min_match()
-            # Reset game
-            turn_dir = os.path.join(self.output_dir, f"matches/{lms[0]}_vs_{lms[1]}_{int(time.time())}")
-            os.makedirs(turn_dir, exist_ok=True)
 
-            game_manager = self._build_game_manager(lms=lms, turn_dir=turn_dir)
+        with ThreadPoolExecutor(max_workers=self.pool_manager_args.get("max_concurrent_matches", 4)) as executor:
+            future_to_match = {}
+            for _ in range(self.max_matches):
+                lms = self.get_next_min_match()
+                future = executor.submit(self.run_match, lms)
+                self.match_freq[tuple(lms)] += 1
+                future_to_match[future] = lms
 
-            assert len(lms) == len(game_manager.games[-1].roles), "The number of matched LMs must be the same as the number of players required in the game."
-            
-            self.logger.info(" VS ".join(lms))
-            
-            final_game_state = game_manager.start()
-            
-            # Get which LM's turn the game stopped at
-            cur_lm_turn = self.roles[final_game_state.roles[0]]
-            gm_message = final_game_state.validate_game()
-            if gm_message in game_manager.win_conditions:
-                result_a = 1 if cur_lm_turn == lms[0] else 0
-                self.llm_wdl[cur_lm_turn]["wins"] += 1
-                self.llm_wdl[self.roles[final_game_state.roles[1]]]["losses"] += 1 
-                self.logger.info(f"Match {cur_lm_turn} won! GM message: {gm_message}")
-            elif gm_message in game_manager.draw_conditions:
-                result_a = 0.5
-                self.llm_wdl[cur_lm_turn]["draws"] += 1
-                self.llm_wdl[self.roles[final_game_state.roles[1]]]["draws"] += 1 
-                self.logger.info(f"Match ended in a draw! GM message: {gm_message}")
-            else:
-                # Loss to current LM because they made the state invalid (exceeded max_tries)
-                result_a = 0 if cur_lm_turn == lms[0] else 1
-                self.llm_wdl[cur_lm_turn]["losses"] += 1
-                self.llm_wdl[self.roles[final_game_state.roles[1]]]["wins"] += 1 
-                self.logger.info(f"Match {cur_lm_turn} lost! GM message: {gm_message}")
-            
-            elos_delta = {}
-            if result_a == 1:
-                elos_delta[lms[0]] = [0, 1]
-                elos_delta[lms[1]] = [0, -1]
-            elif result_a == 0:
-                elos_delta[lms[0]] = [0, -1]
-                elos_delta[lms[1]] = [0, 1]
-            else:
-                elos_delta[lms[0]] = [0, 0]
-                elos_delta[lms[1]] = [0, 0]
+            for future in as_completed(future_to_match):
+                lms, result_a = future.result()
 
-            with open(os.path.join(turn_dir, "results.json"), mode='w', newline='') as f:
-                obj = {
-                    lms[0]: {
-                        "elos_delta": elos_delta[lms[0]],
-                        "result": result_a
-                        
-                    },
-                    lms[1]: {
-                        "elos_delta": elos_delta[lms[1]],
-                        "result": 1 - result_a
-                    }
-                }
-                print(obj)
-                json.dump(obj, f)
-            
-            self.match_freq[tuple(lms)] += 1
-
+                if result_a == 1:
+                    self.llm_wdl[lms[0]]["wins"] += 1
+                    self.llm_wdl[lms[1]]["losses"] += 1
+                elif result_a == 0:
+                    self.llm_wdl[lms[0]]["losses"] += 1
+                    self.llm_wdl[lms[1]]["wins"] += 1
+                else:
+                    self.llm_wdl[lms[0]]["draws"] += 1
+                    self.llm_wdl[lms[1]]["draws"] += 1
 
         return self.llm_wdl
