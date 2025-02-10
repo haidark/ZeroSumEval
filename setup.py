2864f451b54cc3e7f8cb28584843723382fe7990
Haidar Khan <haidark@gmail.com>
2025-02-09 23:22:15 -0500

    setting up pip install

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="zero_sum_eval",
+    name="zero-sum-eval",
     version="0.1.0",
     packages=find_packages(),
     install_requires=[
